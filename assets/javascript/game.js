da5a61e2558f14de80290386a1629c36c13a71a0
Aditi Mishra <asmishra@Aditis-MacBook-Air.local>
2017-12-12 16:03:47 -0600

    clean up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@
 // assign numbers to HTML elements (!)
 // on.click functions that add gem numbers to game number (!)
 // set conditions for game (!)
-// restart game (~)
+// restart game (x)
 
 $(document).ready(function() {
 
@@ -22,25 +22,8 @@ function randomNumberFromRange(min, max) {
     return Math.floor(Math.random()*(max-min+1)+min);
 }
 
-// setting up to start the game
-function initializeGame () {
-
-// starting game number, wins, losses
-    $("#gamenumber").html(randomGameNumber);
-    $("#wins").html("Wins: " + wins);
-    $("#losses").html("Losses: "+ losses);
-    $("#gamescore").html(score);
-    // below is not needed! 
-    //$(".firstnumber").append(randomGem1Number);
-    //$(".secondnumber").append(randomGem2Number);
-    //$(".thirdnumber").append(randomGem3Number);
-    //$(".fourthnumber").append(randomGem4Number);
-}
-
-// add an on click listener for the gems, add random gem number to score
-$(".gem1").click(function(){
-    score += randomGem1Number;
-    $("#gamescore").html(score);
+// function that outlines game rules
+function gameConditions () {
     if (score > randomGameNumber) {
         losses++;
         $("#losses").html("Losses: "+ losses);
@@ -54,80 +37,68 @@ $(".gem1").click(function(){
     else {
         // do nothing
     }
+}
+// setting up to start the game
+function initializeGame () {
+
+// starting game number, wins, losses
+    $("#gamenumber").html(randomGameNumber);
+    $("#wins").html("Wins: " + wins);
+    $("#losses").html("Losses: "+ losses);
+    $("#gamescore").html(score);
+    // below is not needed...
+    //$(".firstnumber").append(randomGem1Number);
+    //$(".secondnumber").append(randomGem2Number);
+    //$(".thirdnumber").append(randomGem3Number);
+    //$(".fourthnumber").append(randomGem4Number);
+}
+
+// add an on click listener for the gems, add random gem number to score
+$(".gem1").click(function(){
+    score += randomGem1Number;
+    $("#gamescore").html(score);
+    
+    gameConditions()
 })
 
 $(".gem2").click(function(){
     score += randomGem2Number;
     $("#gamescore").html(score);
 
-    if (score > randomGameNumber) {
-        losses++;
-        $("#losses").html("Losses: "+ losses);
-        reset()
-    }
-    else if (score === randomGameNumber) {
-        wins++;
-        $("#wins").html("Wins: " + wins);
-        reset()
-    }
-    else {
-        // do nothing
-    }
+    gameConditions()
 })
 
 $(".gem3").click(function(){
     score += randomGem3Number;
     $("#gamescore").html(score);
     
-    if (score > randomGameNumber) {
-        losses++;
-        $("#losses").html("Losses: "+ losses);
-        reset()
-    }
-    else if (score === randomGameNumber) {
-        wins++;
-        $("#wins").html("Wins: " + wins);
-        reset()
-    }
-    else {
-        // do nothing
-    }
+    gameConditions()
 })
 
 $(".gem4").click(function(){
     score += randomGem4Number;
     $("#gamescore").html(score);
 
-    if (score > randomGameNumber) {
-        losses++;
-        $("#losses").html("Losses: "+ losses);
-        reset()
-    }
-    else if (score === randomGameNumber) {
-        wins++;
-        $("#wins").html("Wins: " + wins);
-        reset()
-    }
-    else {
-        // do nothing
-    }
+    gameConditions()
 })
 
 // reset function
+
+
+
+score = 0;
+
 function reset () {
     // regenerate random game number
-    var randomGameNumber = randomNumberFromRange(19, 120);
+    randomGameNumber = randomNumberFromRange(19, 120);
     $("#gamenumber").html(randomGameNumber);
-
     // reset score to zero
-    var score = 0;
-    $("#gamescore").html(score);
-
-    var randomGem1Number = randomNumberFromRange(1, 13); 
-    var randomGem2Number = randomNumberFromRange(1, 12); 
-    var randomGem3Number = randomNumberFromRange(1, 13); 
-    var randomGem4Number = randomNumberFromRange(1, 12);
-
+    score = 0;
+    // reset gem numbers
+    randomGem1Number = randomNumberFromRange(1, 13); 
+    randomGem2Number = randomNumberFromRange(1, 12); 
+    randomGem3Number = randomNumberFromRange(1, 13); 
+    randomGem4Number = randomNumberFromRange(1, 12);
 }
 
 initializeGame()
